9dd39a08bc9e2db69d3b207dea4cb4c9bb0c96df
Nyasha Madeko <131494558+nyashaa17@users.noreply.github.com>
2026-06-03 04:01:36 +0200

    refactor: remove legacy Netlify functions
    Delete unused proxy functions and their associated redirects from the netlify.toml configuration file to clean up the codebase.

--- netlify/functions/purge.ts
+++
@@ -1,9 +0,0 @@
-import { purgeCache } from "@netlify/functions";
-
-export default async () => {
-  console.log("Purging everything");
-
-  await purgeCache();
-
-  return new Response("Purged!", { status: 202 });
-};
