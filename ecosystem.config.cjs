311952e6f6386fde6762a0183145ff0ef19ecb20
Nyasha Madeko <131494558+nyashaa17@users.noreply.github.com>
2026-06-12 12:08:36 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,13 @@
 module.exports = {
-  apps: [
-    {
-      name: "next-app",
-      script: ".next/standalone/server.js",
-      exec_mode: "cluster",
-      instances: "max",
-      env: {
-        NODE_ENV: "production",
-        PORT: 3000,
-      },
-    },
-  ],
-};
+  apps: [{
+    name: 'next-app',
+    script: '.next/standalone/server.js',
+    instances: 'max',
+    exec_mode: 'cluster',
+    env: {
+      PORT: 3000,
+      HOSTNAME: '0.0.0.0',  // 👈 this is the fix
+      NODE_ENV: 'production'
+    }
+  }]
+}
